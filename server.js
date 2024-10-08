780302f5b7c1382bbcd66b906cdb1b790aa94851
krunal vaishnav <krunalvaishnav2004@gmail.com>
2024-10-08 18:33:06 +0530

    create a user model and signin signup home pages

---
+++ server.js
@@ -0,0 +1,45 @@
+`mongoose.set('strictQuery', true);`
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookiePaser = require("cookie-parser");
+
+mongoose.set('strictQuery', false);
+
+const Blog = require("./models/blog");
+
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://localhost:27017/blogiverse")
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookiePaser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
